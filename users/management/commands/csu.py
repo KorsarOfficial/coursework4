be6b8849b0d0985416d8c08b8dc9a71a5b7676a6
Nagaeva Elina <05batman@mail.ru>
2025-03-11 19:45:05 +0400

    создание дефолт пользователя по email

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -5,8 +5,8 @@ from users.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        user = User.objects.create(name="admin")
-        user.set_password("12345")
+        user = User.objects.create(email='admin@mail.ru')
+        user.set_password('12345')
         user.is_active = True
         user.is_staff = True
         user.is_superuser = True
